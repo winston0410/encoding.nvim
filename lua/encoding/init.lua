0ef3fab9a3edef86bdd723c2f6eaeef01b8385d3
nobody <hugosum.dev@protonmail.com>
2025-07-06 22:26:33 +0100

    revert back to original base64

--- lua/encoding/init.lua
+++ lua/encoding/init.lua
@@ -2,7 +2,8 @@ local M = {}
 
 function M.setup()
 	_G.encoding = {
-		base64_encode_or_decode_operator = M.base64_encode_or_decode_operator,
+		base64_encode_operator = M.base64_encode_operator,
+		base64_decode_operator = M.base64_decode_operator,
 		uri_encode_or_decode_operator = M.uri_encode_or_decode_operator,
 	}
 end
@@ -39,18 +40,6 @@ local function select_area_for_operator(mode)
 end
 
 ---@param str string
-local function is_base64_encode(str)
-	if #str % 4 ~= 0 then
-		return false
-	end
-	if not str:match("^[A-Za-z0-9+/]+={0,2}$") then
-		return false
-	end
-
-	return true
-end
-
----@param str string
 local function is_uri_encode(str)
 	if str:find("%%[0-9A-Fa-f][0-9A-Fa-f]") then
 		return true
@@ -61,43 +50,57 @@ end
 function M.uri_encode_or_decode()
 	local mode = vim.api.nvim_get_mode().mode
 	if mode == "v" or mode == "V" or mode == "\22" then
-		M.uri_decode_operator("visual")
+		M.uri_encode_or_decode_operator("visual")
 		return vim.api.nvim_input("<Esc>")
 	end
 	vim.o.opfunc = "v:lua.encoding.uri_encode_or_decode_operator"
 	return vim.api.nvim_input("g@")
 end
 
-function M.base64_encode_or_decode()
+function M.base64_encode()
 	local mode = vim.api.nvim_get_mode().mode
 	-- \22 represents CTRL-V in ASCII
 	if mode == "v" or mode == "V" or mode == "\22" then
 		M.base64_encode_operator("visual")
 		return vim.api.nvim_input("<Esc>")
 	end
-	vim.o.opfunc = "v:lua.encoding.base64_encode_or_decode_operator"
+	vim.o.opfunc = "v:lua.encoding.base64_encode_operator"
 
+	return vim.api.nvim_input("g@")
+end
+function M.base64_decode()
+	local mode = vim.api.nvim_get_mode().mode
+	if mode == "v" or mode == "V" or mode == "\22" then
+		M.base64_decode_operator("visual")
+		return vim.api.nvim_input("<Esc>")
+	end
+	vim.o.opfunc = "v:lua.encoding.base64_decode_operator"
 	return vim.api.nvim_input("g@")
 end
 
 ---@param mode "visual"|nil
-function M.base64_encode_or_decode_operator(mode)
+function M.base64_encode_operator(mode)
 	local start_row, start_col, end_row, end_col = select_area_for_operator(mode)
 
 	local lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
 
 	local text = table.concat(lines, "\n")
 
-	---@type string
-	local result
+	local encoded = vim.base64.encode(text)
 
-	if is_base64_encode(text) then
-		result = vim.base64.decode(text)
-	else
-		result = vim.base64.encode(text)
-	end
+	vim.api.nvim_buf_set_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, { encoded })
+end
+---@param mode "visual"|nil
+function M.base64_decode_operator(mode)
+	local start_row, start_col, end_row, end_col = select_area_for_operator(mode)
 
-	vim.api.nvim_buf_set_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, { result })
+	local lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
+
+	local text = table.concat(lines, "\n")
+
+	local decoded = vim.base64.decode(text)
+
+	vim.api.nvim_buf_set_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, { decoded })
 end
 
 ---@param mode "visual"|nil
