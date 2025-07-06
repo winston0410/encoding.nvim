cc928ddb2dc8471b68ac9123dee35661adee9078
nobody <hugosum.dev@protonmail.com>
2025-07-06 21:04:58 +0100

    update encoding operator

--- lua/encoding/init.lua
+++ lua/encoding/init.lua
@@ -2,10 +2,8 @@ local M = {}
 
 function M.setup()
 	_G.encoding = {
-		base64_encode_operator = M.base64_encode_operator,
-		base64_decode_operator = M.base64_decode_operator,
-		uri_encode_operator = M.uri_encode_operator,
-		uri_decode_operator = M.uri_decode_operator,
+		base64_encode_or_decode_operator = M.base64_encode_or_decode_operator,
+		uri_encode_or_decode_operator = M.uri_encode_or_decode_operator,
 	}
 end
 
@@ -40,91 +38,86 @@ local function select_area_for_operator(mode)
 	return end_row, end_col, start_row, start_col
 end
 
-function M.base64_encode()
+---@param str string
+local function is_base64_encode(str)
+	if #str % 4 ~= 0 then
+		return false
+	end
+	if not str:match("^[A-Za-z0-9+/]+={0,2}$") then
+		return false
+	end
+
+	return true
+end
+
+---@param str string
+local function is_uri_encode(str)
+	if str:find("%%[0-9A-Fa-f][0-9A-Fa-f]") then
+		return true
+	end
+	return false
+end
+
+function M.uri_encode_or_decode()
+	local mode = vim.api.nvim_get_mode().mode
+	if mode == "v" or mode == "V" or mode == "\22" then
+		M.uri_decode_operator("visual")
+		return vim.api.nvim_input("<Esc>")
+	end
+	vim.o.opfunc = "v:lua.encoding.uri_encode_or_decode_operator"
+	return vim.api.nvim_input("g@")
+end
+
+function M.base64_encode_or_decode()
 	local mode = vim.api.nvim_get_mode().mode
 	-- \22 represents CTRL-V in ASCII
 	if mode == "v" or mode == "V" or mode == "\22" then
 		M.base64_encode_operator("visual")
 		return vim.api.nvim_input("<Esc>")
 	end
-	vim.o.opfunc = "v:lua.encoding.base64_encode_operator"
+	vim.o.opfunc = "v:lua.encoding.base64_encode_or_decode_operator"
 
 	return vim.api.nvim_input("g@")
 end
-function M.base64_decode()
-	local mode = vim.api.nvim_get_mode().mode
-	if mode == "v" or mode == "V" or mode == "\22" then
-		M.base64_decode_operator("visual")
-		return vim.api.nvim_input("<Esc>")
+
+---@param mode "visual"|nil
+function M.base64_encode_or_decode_operator(mode)
+	local start_row, start_col, end_row, end_col = select_area_for_operator(mode)
+
+	local lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
+
+	local text = table.concat(lines, "\n")
+
+	---@type string
+	local result
+
+	if is_base64_encode(text) then
+		result = vim.base64.decode(text)
+	else
+		result = vim.base64.encode(text)
 	end
-	vim.o.opfunc = "v:lua.encoding.base64_decode_operator"
-	return vim.api.nvim_input("g@")
+
+	vim.api.nvim_buf_set_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, { result })
 end
-function M.uri_decode()
-	local mode = vim.api.nvim_get_mode().mode
-	if mode == "v" or mode == "V" or mode == "\22" then
-		M.uri_decode_operator("visual")
-		return vim.api.nvim_input("<Esc>")
+
+---@param mode "visual"|nil
+function M.uri_encode_or_decode_operator(mode)
+	local start_row, start_col, end_row, end_col = select_area_for_operator(mode)
+
+	local lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
+
+	local text = table.concat(lines, "\n")
+
+	---@type string
+	local result
+
+	if is_uri_encode(text) then
+		result = vim.uri_decode(text)
+	else
+		result = vim.uri_encode(text)
 	end
-	vim.o.opfunc = "v:lua.encoding.uri_decode_operator"
-	return vim.api.nvim_input("g@")
-end
-function M.uri_encode()
-	local mode = vim.api.nvim_get_mode().mode
-	if mode == "v" or mode == "V" or mode == "\22" then
-		M.uri_encode_operator("visual")
-		return vim.api.nvim_input("<Esc>")
-	end
-	vim.o.opfunc = "v:lua.encoding.uri_encode_operator"
-	return vim.api.nvim_input("g@")
-end
----@param mode "visual"|nil
-function M.base64_encode_operator(mode)
-	local start_row, start_col, end_row, end_col = select_area_for_operator(mode)
 
-	local lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
-
-	local text = table.concat(lines, "\n")
-
-	local encoded = vim.base64.encode(text)
-
-	vim.api.nvim_buf_set_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, { encoded })
-end
----@param mode "visual"|nil
-function M.base64_decode_operator(mode)
-	local start_row, start_col, end_row, end_col = select_area_for_operator(mode)
-
-	local lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
-
-	local text = table.concat(lines, "\n")
-
-	local decoded = vim.base64.decode(text)
-
-	vim.api.nvim_buf_set_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, { decoded })
-end
----@param mode "visual"|nil
-function M.uri_encode_operator(mode)
-	local start_row, start_col, end_row, end_col = select_area_for_operator(mode)
-
-	local lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
-
-	local text = table.concat(lines, "\n")
-
-	local encoded = vim.uri_encode(text, "rfc3986")
-
-	vim.api.nvim_buf_set_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, { encoded })
-end
----@param mode "visual"|nil
-function M.uri_decode_operator(mode)
-	local start_row, start_col, end_row, end_col = select_area_for_operator(mode)
-
-	local lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
-
-	local text = table.concat(lines, "\n")
-
-	local decoded = vim.uri_decode(text)
-
-	vim.api.nvim_buf_set_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, { decoded })
+	vim.api.nvim_buf_set_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, { result })
 end
 
 return M
